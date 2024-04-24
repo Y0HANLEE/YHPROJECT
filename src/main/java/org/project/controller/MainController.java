bd70d4fda17b23597685cc1135bd6505b6650a07
yohanlee <kevinyh@naver.com>
2024-04-25 04:33:52 +0900

    권한수정
    잘못입력했던 권한 조정

--- src/main/java/org/project/controller/MainController.java
+++ src/main/java/org/project/controller/MainController.java
@@ -2,6 +2,7 @@ package org.project.controller;
 
 import org.project.service.IntroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Log4j
 @Controller
 @RequestMapping("/main/*")
 public class MainController {	
     @Setter(onMethod_=@Autowired)
     private IntroService iservice;
-	
+	    
     @GetMapping({"/home","/intro"})
     public void introPage(Model model) {    	
     	model.addAttribute("home", iservice.read(1));
