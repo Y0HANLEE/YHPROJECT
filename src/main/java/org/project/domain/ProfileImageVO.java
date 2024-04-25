b5ddae3310d475fc0c6dd987386d4859efdb5d7a
yohanlee <kevinyh@naver.com>
2024-04-26 07:38:49 +0900

    타입변경

--- src/main/java/org/project/domain/ProfileImageVO.java
+++ src/main/java/org/project/domain/ProfileImageVO.java
@@ -7,6 +7,6 @@ public class ProfileImageVO {
 	private String uuid;		//pk, not null
 	private String fileName;	//not null
 	private String uploadPath;	//not null
-	private String fileType;	//default 'I'
+	private boolean fileType;	//default 'I'
 	private String userid;		//fk, not null
 }
