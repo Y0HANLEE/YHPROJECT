aade52494616f5a277a82de82ba65ce07c90e493
yohanlee <kevinyh@naver.com>
2024-05-12 07:04:52 +0900

    pw생성기
    관리자 pw생성

---
+++ src/main/java/org/project/controller/PwEncoder.java
@@ -0,0 +1,17 @@
+package org.project.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PwEncoder {
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 10) {
+			String password = "1234";
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			String hashedPassword = passwordEncoder.encode(password);
+
+			System.out.println(hashedPassword);
+			i++;
+		}
+	}
+}
