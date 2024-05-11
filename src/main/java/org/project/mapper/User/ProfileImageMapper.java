5f5c45a4516b409af3ae51a92772b89d6a258625
yohanlee <kevinyh@naver.com>
2024-05-12 07:11:10 +0900

    getOldFiles 추가

--- src/main/java/org/project/mapper/User/ProfileImageMapper.java
+++ src/main/java/org/project/mapper/User/ProfileImageMapper.java
@@ -1,5 +1,7 @@
 package org.project.mapper.User;
 
+import java.util.List;
+
 import org.project.domain.User.ProfileImageVO;
 
 public interface ProfileImageMapper {
@@ -7,8 +9,9 @@ public interface ProfileImageMapper {
 	public void insert(ProfileImageVO profile);
 	//조회
 	public ProfileImageVO getProfileByUserid(String userid);
+	public List<ProfileImageVO> getOldFiles(); // 하루전 첨부파일목록 조회 | 비교 후 삭제를 위함
 	//수정
 	public int update(ProfileImageVO profile);	
 	//삭제
-	public int delete(String userid);
+	public int delete(String userid);	
 }
