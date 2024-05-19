54a4a61c3041854c8c08cc5b5982b78b9bf96678
yohanlee <145434043+Y0HANLEE@users.noreply.github.com>
2024-05-19 15:50:53 +0900

    Update PwGenerator.java

--- src/main/java/org/project/controller/PwGenerator.java
+++ src/main/java/org/project/controller/PwGenerator.java
@@ -2,7 +2,7 @@ package org.project.controller;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class PwEncoder {
+public class PwGenerator {
 	public static void main(String[] args) {
 		int i = 0;
 		while (i < 10) {
